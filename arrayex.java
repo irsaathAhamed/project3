33fd5a903fe8e0e7b529a4cd06297d347e6d3f78
irsaathAhamed <102521397+irsaathAhamed@users.noreply.github.com>
2022-03-29 12:24:56 +0530

    Update arrayex.java

--- arrayex.java
+++ arrayex.java
@@ -8,8 +8,9 @@ int [] array = new int [] {90, 23, 5, 109, 12, 22, 67, 34};
 //invoking sort() method of the Arrays class  
 Arrays.sort(array);   
 System.out.println("Elements of array sorted in ascending order: ");  
-//prints array using the for loop  
-for (int i = 0; i < array.length; i++)   
+//prints array using the for loop 
+  //gprfgfhudfgb
+for (int i = 0; j < array.length; i++)   
 {       
 System.out.println(array[i]);   
 }   
